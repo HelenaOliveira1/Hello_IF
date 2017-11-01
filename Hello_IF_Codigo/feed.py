c30b0b1b44ba8415fe4caceda1ca3c07cd57fbd0
Eduarda Alves <Eduarda8@users.noreply.github.com>
2017-11-01 09:03:15 -0200

    Update feed.py

--- Hello_IF_Codigo/feed.py
+++ Hello_IF_Codigo/feed.py
@@ -2,3 +2,71 @@
     DML da tabela feed
 """
 
+import sqlite3
+
+conn = sqlite3.connect('hello_if.db')
+
+cursor = conn.cursor()
+
+def inserir_dados_feed():
+    try:
+        visib_mens = str(input("Digite tipo da publicação: "))
+
+        cursor.execute("""
+        INSERT INTO tb_publicacao (visib_mens)
+        VALUES ('?','?','?') """, (visib_mens))
+
+        # Salvando...
+        conn.commit()
+        print("Um registro inserido com sucesso.")
+
+    except sqlite3.Error:
+        print("Ocorreu um ERRO!")
+        return False
+
+def lendo_imprimindo_feed():
+    cursor.execute("""
+    SELECT * FROM tb_feed;
+    """)
+
+    for linha in cursor.fetchall():
+        print(linha)
+
+    # Salvando...
+    conn.commit()
+    print("Lemos com sucesso.")
+
+def alterar_dados_feed():
+    try:
+        novo_tipo = str(input("Digite novo tipo da publicação: "))
+
+        cursor.execute("""
+            UPDATE tb_feed
+            SET novo_tipo=?
+            WHERE visib_mens = ?
+            """, (novo_tipo))
+
+        # Salvando...
+        conn.commit()
+        print("Registro alterado com sucesso.")
+
+    except sqlite3.Error:
+        print("Ocorreu um ERRO!")
+        return False
+
+def deletar_dados_feed():
+    try:
+        tipo_publicacao = int(input("Digite o tipo da publicação para remover: "))
+        cursor.execute("""
+            DELETE FROM tb_publicacao
+            WHERE visib_mens = ?
+            """, (tipo_publicacao))
+
+        # Salvando...
+        conn.commit()
+        print("Registro deletado com sucesso.")
+
+    except sqlite3.Error:
+        print("Ocorreu um ERRO!")
+        return False
+
