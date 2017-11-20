5123515490e9cf986846f902d2fe94e13457f36b
HelenaOliveira1 <helenna54319@gmail.com>
2017-11-20 11:01:39 -0200

    Corrigindo erros graves

--- app.py
+++ app.py
@@ -9,50 +9,68 @@ def main(Args = []):
     from Model.usuario import Usuario
     from Model.amigo import Amigo
 
-    print("==========   Bem vindo a nossa Rede Social!   ==========")
-    op = int(input("==========  MENU  ==========\n 1 - Criar outra Rede Social\n 2 - Sair\n"))
+    cont = False
 
-    while (op != 1 and op != 2):
-        print ("Opção não existe.")
-        op = int(input("==========  MENU  ==========\n 1 - Criar outra Rede Social\n 2 - Sair\n"))
+    print("==========   Bem vindo a nossa Rede Social!   ==========\n")
+
+    op  = ''
+
+    while(True):
+        try:
+            op = int(input("==========  MENU  ==========\n 1 - Criar ou entrar na Rede Social\n 2 - Sair\n"))
+            if (op != 1 and op != 2):
+                print("Número inválido")
+            else:
+                break
+        except ValueError:
+            print("Opção inválida")
+
 
     if (op == 1):
 
         nome = str(input("Digite o nome da sua rede social: "))
-        
-        conn = sqlite3.connect('hello_if.db')
-        cursor = conn.cursor()
-        
-        cursor.execute("""
-        CREATE TABLE tb_usuario (
-    	    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    	    senha VARCHAR(25) NOT NULL,
-    	    login VARCHAR(50) NOT NULL,
-    	    logado BOOLEAN,
-    	    nome VARCHAR(70) NOT NULL,
-    	    data_nasc DATE,
-    	    genero VARCHAR(10),
-    	    profissao VARCHAR(20));
-    	    """)
-        
-        cursor.execute("""
-        CREATE TABLE tb_amigo (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-    	    id_usuario INTEGER NOT NULL,
-    	    id_usuario_amigo INTEGER NOT NULL,
-    	    foreign key (id_usuario) references tb_usuario(id),
-    	    foreign key (id_usuario_amigo) references tb_usuario(id));
-    	    """)
 
-        conn.commit()
+        try:
 
-        print("Sua rede social '%s' foi criada com sucesso!" %nome)
 
-        op2 = int(input("==========  MENU  ==========\n1 - Criar usuário\n 2 - Adicionar Amigo\n 4 - Desfazer Amizade\n 5 - Realizar Busca\n 0 - Sair\n"))
+
+            conn = sqlite3.connect('%s.db'%nome)
+            cursor = conn.cursor()
+
+            cursor.execute("""
+            CREATE TABLE tb_usuario (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                senha VARCHAR(25) NOT NULL,
+                login VARCHAR(50) NOT NULL,
+                logado BOOLEAN,
+                nome VARCHAR(70) NOT NULL,
+                data_nasc DATE,
+                genero VARCHAR(10),
+                profissao VARCHAR(20));
+                """)
+
+            cursor.execute("""
+            CREATE TABLE tb_amigo (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                id_usuario INTEGER NOT NULL,
+                id_usuario_amigo INTEGER NOT NULL,
+                foreign key (id_usuario) references tb_usuario(id),
+                foreign key (id_usuario_amigo) references tb_usuario(id));
+                """)
+
+            conn.commit()
+
+            print("Sua rede social '%s' foi criada com sucesso!" %nome)
+        except:
+            print("Conexão bem sucedida com a rede social %s\n " %nome)
+
+
+
+        op2 = int(input("==========  MENU  ==========\n1 - Criar usuário\n 2 - Adicionar Amigo\n 3 - Desfazer Amizade\n 4 - Realizar Busca\n 0 - Sair\n"))
         cond = True
 
         while (cond):
-            while (op2 != 1 and op2 != 2 and op2 != 3):
+            while (op2 != 1 and op2 != 2 and op2 != 3 and op2 != 4 and op2 != 5 and op2 != 0):
                 print("Opção não existe.")
                 op2 = int(input("==========  MENU  ==========\n1 - Criar usuário\n 2 - Adicionar Amigo\n 3 - Sair\n"))
 
@@ -70,9 +88,11 @@ def main(Args = []):
                 genero = str(input("Digite seu genero: "))
                 profissao = str(input("Digite sua profissao: "))
                 Usuario(id, senha, login, logado, nome, data_nasc, genero, profissao)
+
                 cursor.execute('''
-                        INSERT INTO tb_usuario(senha, login, logado, nome, data_nasc, genero, profissao) VALUES (senha, login, logado,nome,data_nasc,genero,profissao)
-                                ''')
+                        INSERT INTO tb_usuario(senha, login, logado, nome, data_nasc, genero, profissao) 
+                        VALUES (?,?,?,?,?,?,?)
+                                ''',(senha, login, logado, nome, data_nasc, genero, profissao))
 
                 conn.commit()
                 print("Criado com sucesso!")
@@ -100,15 +120,14 @@ def main(Args = []):
 
                     print("Ocorreu um ERRO!...tente novamente mais tarde.")
                     return False
-
-                op2 = int(input("==========  MENU  ==========\n 1 - Criar usuário\n 2 - Adicionar Amigo\n 3 - Enviar mensagem\n 4 - Desfazer Amizade\n 5 - Realizar Busca\n 0 - Sair\n"))
+                    op2 = int(input("==========  MENU  ==========\n 1 - Criar usuário\n 2 - Adicionar Amigo\n 3 - Enviar mensagem\n 4 - Desfazer Amizade\n 5 - Realizar Busca\n 0 - Sair\n"))
                 
             elif (op2 == 3):
                 Usuario.enviarDM()
-                
+
             elif (op2 == 4):
                 Usuario.desfazerAmizade()
-                
+
             elif (op2 == 5):
                 nome = str(input("Digite o nome do amigo: "))
                 Usuario.realizarBusca(nome)
@@ -117,6 +136,9 @@ def main(Args = []):
                 print("Saindo...\nOBS.: Todos os dados serão perdidos.")
                 cond = False
                 conn.close()
+
+            else:
+                print("Opção inválida")
 
     elif (op == 2):
         print("Saindo...")
