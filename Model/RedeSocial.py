caf6482745803c54f2c49d9b362c7308f2170507
HelenaOliveira1 <helenna54319@gmail.com>
2017-12-07 08:01:54 -0300

    Update RedeSocial.py

--- Model/RedeSocial.py
+++ Model/RedeSocial.py
@@ -3,15 +3,8 @@
 '''
 
 class RedeSocial():
-
-    def __init__(self, , senha = None, login = None, logado = None, nome, data_nasc = None, genero, profissao):
-        self.senha = senha
-        self.login = login
-        self.logado = logado
+    def __init__(self, nome):
         self.nome = nome
-        self.data_nasc = data_nasc
-        self.genero = genero
-        self.profissao = profissao
-
+        
     def __str__(self):
         return "Rede Social <%s>"%(self.nome)
