6ae889496a7116e49ca9ee10f6cb633ded0304b5
Eduarda Alves <Eduarda8@users.noreply.github.com>
2017-12-06 22:59:08 -0200

    Update Rede Social

--- Model/RedeSocial.py
+++ Model/RedeSocial.py
@@ -4,8 +4,14 @@
 
 class RedeSocial():
 
-    def __init__(self, nome):
+    def __init__(self, , senha = None, login = None, logado = None, nome, data_nasc = None, genero, profissao):
+        self.senha = senha
+        self.login = login
+        self.logado = logado
         self.nome = nome
+        self.data_nasc = data_nasc
+        self.genero = genero
+        self.profissao = profissao
 
     def __str__(self):
         return "Rede Social <%s>"%(self.nome)
