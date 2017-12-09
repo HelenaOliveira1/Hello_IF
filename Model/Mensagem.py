4335a296f8c9a0890d9959eeb8eb151a53633c5e
HelenaOliveira1 <helenna54319@gmail.com>
2017-12-08 23:47:17 -0200

    Update Mensagem.py

--- Model/Mensagem.py
+++ Model/Mensagem.py
@@ -16,50 +16,61 @@ class Mensagem():
         self.texto = texto
 
     def listar(self):
-        mensagens = []
+        try:
+            mensagens = []
 
-        conn = mysql.connector.connect(**config)
-        cursor = conn.cursor()
+            conn = mysql.connector.connect(**config)
+            cursor = conn.cursor()
 
-        cursor.execute('''
-            SELECT * FROM tb_mensagem;
-        ''')
-        for linha in cursor.fechall():
-            visibilidade = linha[1]
-            texto = linha[2]
-            mensagem = Mensagem(visibilidade, texto)
-            mensagens.append(mensagem)
-
-        cursor.close()
-        conn.close()
-
-        return mensagens
+            cursor.execute('''
+                SELECT * FROM tb_mensagem;
+            ''')
+            for linha in cursor.fechall():
+                visibilidade = linha[1]
+                texto = linha[2]
+                mensagem = Mensagem(visibilidade, texto)
+                mensagens.append(mensagem)
+            
+            return mensagens
+        except:
+           print("Ocorreu um ERRO!")
+        finally:
+            cursor.close()
+            conn.close()
 
     def inserir(self):
+        try:
 
-        conn = mysql.connector.connect(**config)
-        cursor = conn.cursor()
+            conn = mysql.connector.connect(**config)
+            cursor = conn.cursor()
 
-        cursor.execute('''
-            INSERT INTO tb_mensagem(visibilidade, texto) VALUES (?,?)
-        ''', (self.visibilidade, self.texto))
+            cursor.execute('''
+                INSERT INTO tb_mensagem(visibilidade, texto) VALUES (?,?)
+            ''', (self.visibilidade, self.texto))
 
-        conn.commit()
-        id = cursor.lastrowid
-        cursor.close()
-        conn.close()
-
-        return id
+            conn.commit()
+            id = cursor.lastrowid
+            
+            return id
+        except:
+            print("Ocorreu um ERRO!")
+        finally;
+            cursor.close()
+            conn.close()
 
     def deletar(self, id_mens):
+        try:
 
-        conn = mysql.connector.connect(**config)
-        cursor = conn.cursor()
-        cursor.execute(''' DELETE FROM tb_mensagem WHERE id =? ''', id_mens)
+            conn = mysql.connector.connect(**config)
+            cursor = conn.cursor()
+            cursor.execute(''' DELETE FROM tb_mensagem WHERE id =? ''', id_mens)
 
-        conn.commit()
-        cursor.close()
-        conn.close()
+            conn.commit()
+        except:
+            print("Ocorreu um ERRO!")
+        finally:
+            cursor.close()
+            conn.close()
 
     def __str__(self):
         return "Mensagem <%i>" %(self.id)
