64680d62f65932be4f28c6f49c19f22543e6e900
HelenaOliveira1 <helenna54319@gmail.com>
2017-12-08 23:49:22 -0200

    Update Publicacao.py

--- Model/Publicacao.py
+++ Model/Publicacao.py
@@ -14,48 +14,58 @@ class Publicacao():
         self.tipo = tipo
 
     def listar(self):
-        publicacoes = []
+        try:
+            publicacoes = []
 
-        conn = mysql.connector.connect(**config)
-        cursor = conn.cursor()
+            conn = mysql.connector.connect(**config)
+            cursor = conn.cursor()
 
-        cursor.execute('''
-            SELECT * FROM tb_publicacao;
-        ''')
-        for linha in cursor.fechall():
-            tipo = linha[1]
-            publicacao = Publicacao(tipo)
-            publicacoes.append(publicacao)
-
-        cursor.close()
-        conn.close()
-
-        return publicacoes
+            cursor.execute('''
+                SELECT * FROM tb_publicacao;
+            ''')
+            for linha in cursor.fechall():
+                tipo = linha[1]
+                publicacao = Publicacao(tipo)
+                publicacoes.append(publicacao)
+            return publicacoes
+        except:
+            print("Ocorreu um ERRO!")
+        finally:
+            cursor.close()
+            conn.close()
 
     def inserir(self):
+        try:
 
-        conn = mysql.connector.connect(**config)
-        cursor = conn.cursor()
+            conn = mysql.connector.connect(**config)
+            cursor = conn.cursor()
 
-        cursor.execute(''' INSERT INTO tb_publicacao(tipo) VALUES (?) ''', self.tipo)
+            cursor.execute(''' INSERT INTO tb_publicacao(tipo) VALUES (?) ''', self.tipo)
 
-        conn.commit()
-        id = cursor.lastrowid
-        cursor.close()
-        conn.close()
-
-        return id
+            conn.commit()
+            id = cursor.lastrowid
+            
+            return id
+        except:
+            print("Ocorreu um ERRO!")
+        finally:
+            cursor.close()
+            conn.close()
 
     def deletar(self, id_mens):
+        try:
 
-        conn = mysql.connector.connect(**config)
-        cursor = conn.cursor()
+            conn = mysql.connector.connect(**config)
+            cursor = conn.cursor()
 
-        cursor.execute(''' DELETE FROM tb_publicacao WHERE id =? ''', (id_mens))
+            cursor.execute(''' DELETE FROM tb_publicacao WHERE id =? ''', (id_mens))
 
-        conn.commit()
-        cursor.close()
-        conn.close()
+            conn.commit()
+        except:
+            print("Ocorreu um ERRO!")
+        finally:
+            cursor.close()
+            conn.close()
 
     def __str__(self):
         return "Publicação <%i>" %(self.id)
