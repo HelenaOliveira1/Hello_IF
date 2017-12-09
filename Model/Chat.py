b7bd5eb9dff92275d478ddee9910c1a6f61d24a8
HelenaOliveira1 <helenna54319@gmail.com>
2017-12-08 23:42:31 -0200

    Update Chat.py

--- Model/Chat.py
+++ Model/Chat.py
@@ -21,51 +21,64 @@ class Chat():
         self.mensagem = Mensagem(id_m, visibilidade, texto)
 
     def listar(self):
-        mensagens = []
+        try:
+            mensagens = []
 
-        conn = mysql.connector.connect(**config)
-        cursor = conn.cursor()
+            conn = mysql.connector.connect(**config)
+            cursor = conn.cursor()
 
-        cursor.execute('''
-            SELECT * FROM tb_mensagem;
-        ''')
-        for linha in cursor.fechall():
-            usuario = linha[1]
-            amigo = linha[2]
-            mensagem = linha[3]
-            lista_mensagens = Chat(usuario, amigo, mensagem)
-            mensagens.append(lista_mensagens)
+            cursor.execute('''
+                SELECT * FROM tb_mensagem;
+            ''')
+            for linha in cursor.fechall():
+                usuario = linha[1]
+                amigo = linha[2]
+                mensagem = linha[3]
+                lista_mensagens = Chat(usuario, amigo, mensagem)
+                mensagens.append(lista_mensagens)
 
-        cursor.close()
-        conn.close()
-
-        return mensagens
+            return mensagens
+        
+        except:
+            print("Ocorreu um ERRO!")
+            
+        finally:
+            cursor.close()
+            conn.close()
 
     def inserir(self):
+        try:
 
-        conn = mysql.connector.connect(**config)
-        cursor = conn.cursor()
-        cursor.execute('''
-            INSERT INTO tb_mensagem(id, id_usuario, visib_mens, id_usuario_amigo) VALUES (?,?,?,?)
-        ''',(self.id, self.usuario.id_u, self.mensagem.visibilidade, self.amigo.id_a))
+            conn = mysql.connector.connect(**config)
+            cursor = conn.cursor()
+            cursor.execute('''
+                INSERT INTO tb_mensagem(id, id_usuario, visib_mens, id_usuario_amigo) VALUES (?,?,?,?)
+            ''',(self.id, self.usuario.id_u, self.mensagem.visibilidade, self.amigo.id_a))
 
-        conn.commit()
-        id = cursor.lastrowid
-        cursor.close()
-        conn.close()
-
-        return id
+            conn.commit()
+            id = cursor.lastrowid
+            return id
+        except:
+            print("Ocorreu um ERRO!")
+            
+        finally:
+            cursor.close()
+            conn.close()
 
     def deletar(self, id_delt):
+        try:
 
-        conn = mysql.connector.connect(**config)
-        cursor = conn.cursor()
+            conn = mysql.connector.connect(**config)
+            cursor = conn.cursor()
 
-        cursor.execute(''' DELETE FROM tb_chat WHERE id =? ''', id_delt)
+            cursor.execute(''' DELETE FROM tb_chat WHERE id =? ''', id_delt)
 
-        conn.commit()
-        cursor.close()
-        conn.close()
+            conn.commit()
+        except:
+            print("Ocorreu um ERRO!")
+        finally:
+            cursor.close()
+            conn.close()
 
     def __str__(self):
         return "Chat <%i>" %(self.id)
