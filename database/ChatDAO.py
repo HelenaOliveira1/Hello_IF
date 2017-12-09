8d0b9ffcbd3e98f74b87b2f366426781f1766bfd
HelenaOliveira1 <helenna54319@gmail.com>
2017-12-08 23:27:15 -0200

    Update ChatDAO.py

--- database/ChatDAO.py
+++ database/ChatDAO.py
@@ -17,27 +17,34 @@ def inserir_dados_chat():
         conn.commit()
         print("Um registro inserido com sucesso.")
 
-    except mysql.Error:
+    except mysql.connector.Error as error:
+        print(error)
         print("Ocorreu um ERRO!")
         return False
+    
+    finally:
+        cursor.close()
+        conn.close()
 
-    cursor.close()
-    conn.close()
+def lendo_imprimindo_chat():   
+    try:
+        cursor.execute("""
+        SELECT * FROM tb_chat;
+        """)
 
-def lendo_imprimindo_chat():
-    cursor.execute("""
-    SELECT * FROM tb_chat;
-    """)
+        for linha in cursor.fetchall():
+            print(linha)
 
-    for linha in cursor.fetchall():
-        print(linha)
+        # Salvando...
+        conn.commit()
+        print("Lemos com sucesso.")
+        
+    except:
+        print("Ocorreu um ERRO!")
 
-    # Salvando...
-    conn.commit()
-    print("Lemos com sucesso.")
-
-    cursor.close()
-    conn.close()
+    finally:
+        cursor.close()
+        conn.close()
 
 def alterar_dados_chat():
     try:
@@ -53,12 +60,14 @@ def alterar_dados_chat():
         conn.commit()
         print("Registro alterado com sucesso.")
 
-    except mysql.Error:
+    except mysql.connector.Error as error:
+        print(error)
         print("Ocorreu um ERRO!")
         return False
 
-    cursor.close()
-    conn.close()
+    finally:
+        cursor.close()
+        conn.close()
 
 def deletar_dados_chat():
     try:
@@ -72,9 +81,11 @@ def deletar_dados_chat():
         conn.commit()
         print("Registro deletado com sucesso.")
 
-    except mysql.Error:
+    except mysql.connector.Error as error:
+        print(error)
         print("Ocorreu um ERRO!")
         return False
-
-    cursor.close()
-    conn.close()
+    
+    finally:
+        cursor.close()
+        conn.close()
