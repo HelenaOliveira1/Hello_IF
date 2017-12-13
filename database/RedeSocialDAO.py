4a1f5190f8193ad90047e9e6689d3be9dbf9e95a
Eduarda Alves <Eduarda8@users.noreply.github.com>
2017-12-13 19:19:18 -0200

    Organizando o código

--- database/RedeSocialDAO.py
+++ database/RedeSocialDAO.py
@@ -2,14 +2,14 @@
     DML da Rede Social
 '''
 
-import mysql
+import mysql.connector
 from Model.RedeSocial import RedeSocial
 from database.Tabelas import *
 
 class RedeSocialDAO():
-
     def criarRedeSocial(redesocial: RedeSocial):
-
+        
+        # Conectando com o Banco e definindo o cursor
         conn = mysql.connect('%s.db'%redesocial.nome)
         cursor = conn.cursor()
 
@@ -19,7 +19,7 @@ class RedeSocialDAO():
             print("Sua rede social '%s' foi criada com sucesso!" %redesocial.nome)
         except:
             print("Conexão bem sucedida com a rede social %s\n" %redesocial.nome)
-            
+        # Finalizando as operações
         finally:
             cursor.close()
             conn.close()
