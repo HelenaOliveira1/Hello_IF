14746abc50beb657fc9d5a703fb7812c77b9b5b0
Eduarda Alves <Eduarda8@users.noreply.github.com>
2017-12-13 14:58:33 -0300

    Organizando o código

--- database/AmigoDAO.py
+++ database/AmigoDAO.py
@@ -4,44 +4,48 @@
 
 import mysql.connector
 from database.ConfigDB import config
-from Model.Amigo import *
+from Model.Amigo import Amigo
 
 class AmigoDAO():
     def inserir(self):
+        # Tratando os possiveis erros
         try:
+            # Conectando com o Banco e definindo o cursor
             conn = mysql.connector.connect(**config)
             cursor = conn.cursor()
+            # Inserindo dados na tabela Amigo
             cursor.execute('''
-                INSERT INTO tb_amigo(id, id_usuario, id_usuario_amigo) VALUES (?,?,?)
-            ''', (self.id, self.usuario.id_u, self.amigo.id_a))
-
+                INSERT INTO tb_amigo(id, id_usuario, id_usuario_amigo) 
+                VALUES (?,?,?)''', (self.id, self.usuario.id_u, self.amigo.id_a))
+            
+            # Salvando...
             conn.commit()
             id = cursor.lastrowid
 
             return id
-
+        
         except mysql.connector.Error as error:
             print(error)
             print("Ocorreu um ERRO!")
             return False
-
+        
+        # Finalizando as operações
         finally:
             cursor.close()
             conn.close()
 
-
     def listar(self):
-
         try:
             conn = mysql.connector.connect(**config)
             cursor = conn.cursor()
             amigos = []
-
-            conn = mysql.connector.connect(**config)
-            cursor = conn.cursor()
+            
+            # Selecionando tudo da tabela Amigo
             cursor.execute('''
                 SELECT * FROM tb_amigo;
             ''')
+            
+            # Imprimindo resultados
             for linha in cursor.fechall():
                 usuario = linha[1]
                 amigo = linha[2]
@@ -58,10 +62,11 @@ class AmigoDAO():
             conn.close()
 
     def deletar(self, id_delt):
-
         try:
             conn = mysql.conncetor.connect(**config)
             cursor = conn.cursor()
+            
+            # Deletando através do id uma tupla da tabela Amigo
             cursor.execute('''
                 DELETE FROM tb_amigo
                 WHERE id =?
