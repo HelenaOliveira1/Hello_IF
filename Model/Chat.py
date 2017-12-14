ac9140c79b4b6e0f1f2ca52f8cf5fa6a9f9fff5e
HelenaOliveira1 <helenna54319@gmail.com>
2017-12-14 09:12:02 -0200

    Organizando

--- Model/Chat.py
+++ Model/Chat.py
@@ -2,19 +2,18 @@
     Chat Orientado a Objeto
 """
 
-
 from Model.Usuario import Usuario
 from Model.Mensagem import Mensagem
 from database.ChatDAO import ChatDAO
 
 class Chat():
     #Inicializando o objeto Chat
-    def __init__(self, id, id_u, senha_u, login_u, logado_u, nome_u, data_nasc_u, genero_u, profissao_u, id_a, senha_a,
-                 login_a, logado_a, nome_a, data_nasc_a, genero_a, profissao_a, id_m, visibilidade, texto):
+    def __init__(self, id, senha_u, login_u, logado_u, nome_u, data_nasc_u, genero_u, profissao_u, senha_a,
+                 login_a, logado_a, nome_a, data_nasc_a, genero_a, profissao_a, visibilidade, texto):
         self.id = id
-        self.usuario = Usuario(id_u, senha_u, login_u, logado_u, nome_u, data_nasc_u, genero_u, profissao_u)
-        self.amigo = Usuario(id_a, senha_a, login_a, logado_a, nome_a, data_nasc_a, genero_a, profissao_a)
-        self.mensagem = Mensagem(id_m, visibilidade, texto)
+        self.usuario = Usuario(senha_u, login_u, logado_u, nome_u, data_nasc_u, genero_u, profissao_u)
+        self.amigo = Usuario(senha_a, login_a, logado_a, nome_a, data_nasc_a, genero_a, profissao_a)
+        self.mensagem = Mensagem(visibilidade, texto)
 
     #Função para listar o Chat
     def listar(self):
@@ -36,4 +35,3 @@ class Chat():
     #Alterando o método representativo de Chat
     def __repr__(self):
         return self.__str__()
-
