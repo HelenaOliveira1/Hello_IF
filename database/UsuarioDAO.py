0a195ad95e7d2206795142f153bb6ee6a057f27e
HelenaOliveira1 <helenna54319@gmail.com>
2017-12-14 09:47:53 -0200

    Update UsuarioDAO.py

--- database/UsuarioDAO.py
+++ database/UsuarioDAO.py
@@ -2,10 +2,11 @@
     DML da tabela usuario
 """
 
-import mysql.connector
-from Model.Usuario import Usuario
-from database.ConfigDB import *
 import datetime
+import mysql.connector
+from database.ConfigDB import *
+from Model.Usuario import Usuario
+from database.AmigoDAO import AmigoDAO
 
 class UsuarioDAO():
     def inserir(self, usuario):
@@ -150,3 +151,134 @@ class UsuarioDAO():
         finally:
             cursor.close()
             conn.close()
+
+    def realizarBusca(self, nome):
+        try:
+
+            conn = mysql.connector.connect(**config)
+            cursor = conn.cursor()
+
+            cursor.execute(''' 
+                SELECT * FROM tb_usuario WHERE nome LIKE '%?%' ''', (nome))
+
+            for linha in cursor.fetchall():
+                print("%s\n", linha)
+
+            conn.commit()
+
+        except:
+            print("Ocorreu um ERRO!")
+        finally:
+            cursor.close()
+            conn.close()
+
+    def postPubPrivada(self):
+        try:
+            conn = mysql.connector.connect(**config)
+            cursor = conn.cursor()
+
+            texto = str(input("Digite o texto para ser enviado: "))
+            usuario = str(input("Digite o nome do destinatário da publicação: "))
+
+            cursor.execute('''
+                SELECT nome FROM tb_usuario WHERE nome=? ''', (usuario))
+
+            # Enviando mensagem...
+            cursor.execute(''' 
+                UPDATE tb_publicacao SET texto=? ''', (texto))
+
+            print("Publicação enviada com sucesso.")
+
+            conn.commit()
+
+        except:
+            print("Ocorreu um ERRO!")
+        finally:
+            cursor.close()
+            conn.close()
+
+    def postPubPublica(self):
+        try:
+            conn = mysql.connector.connect(**config)
+            cursor = conn.cursor()
+
+            texto = str(input("Digite o texto para ser enviado: "))
+
+            cursor.execute('''
+                SELECT nome FROM tb_usuario''')
+
+            #Enviando mensagem...
+            cursor.execute('''
+                UPDATE tb_publicacao SET texto=? ''', (texto))
+
+            print("Publicação enviada com sucesso.")
+
+            conn.commit()
+
+        except:
+            print("Ocorreu um ERRO!")
+        finally:
+            cursor.close()
+            conn.close()
+
+    def enviarDM(self):
+        try:
+            conn = mysql.connector.connect(**config)
+            cursor = conn.cursor()
+
+            texto = str(input("Digite o texto para ser enviado: "))
+            usuario = str(input("Digite o nome do destinatário da mensagem: "))
+
+            cursor.execute('''
+                SELECT nome FROM tb_usuario WHERE nome=? ''', (usuario))
+
+            #Enviando mensagem...
+            cursor.execute('''
+                UPDATE tb_mensagem SET texto=? ''', (texto))
+
+            print("Mensagem enviada com sucesso.")
+
+            conn.commit()
+
+        except:
+            print("Ocorreu um ERRO!")
+        finally:
+            cursor.close()
+            conn.close()
+
+    def desfazerAmizade(self):
+        try:
+            opcao = int(input("Digite 1 para deletar amigo pelo id ou 2 para deletar o amigo pelo nome: "))
+
+            if (opcao == 1):
+
+                id_delt = int(input("Digite o id do seu amigo: "))
+                AmigoDAO.deletar(id_delt)
+
+            elif (opcao == 2):
+
+                nome_amigo = str(input("Digite o nome do seu amigo que deseja desfazer amizade:"))
+
+                conn = mysql.connector.connect(**config)
+                cursor = conn.cursor()
+
+                cursor.execute('''
+                    DELETE FROM tb_amigo WHERE nome =? ''', (nome_amigo))
+
+            else:
+                print("Opção não existente.")
+
+            conn.commit()
+
+        except:
+            print("Ocorreu um ERRO!")
+        finally:
+            cursor.close()
+            conn.close()
+
+    def fazerAmizade(self):
+        try:
+            AmigoDAO.inserir()
+
+        except:
+            print("Ocorreu um ERRO!")
